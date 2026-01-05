166f37078bd82713b8e3ef9f451eb3e45da937b1
Bahar Hamzeh <blikebahar76@gmail.com>
2026-01-04 23:32:06 -0600

    chore: add placeholder Storybook configuration files for future compatibility with Next.js 16

---
+++ .storybook/main.ts
@@ -0,0 +1,25 @@
+// TODO: Uncomment this entire file when Storybook supports Next.js 16
+// When uncommenting, also:
+// 1. Install Storybook dependencies: pnpm add -D storybook @storybook/nextjs @storybook/addon-essentials @storybook/addon-themes @storybook/addon-onboarding @chromatic-com/storybook @storybook/experimental-addon-test
+// 2. Remove the exclusion of "stories" from tsconfig.json
+// 3. Uncomment the Storybook scripts in package.json
+
+// import type { StorybookConfig } from "@storybook/nextjs";
+
+// const config: StorybookConfig = {
+//   stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+//   addons: [
+//     "@storybook/addon-essentials",
+//     "@storybook/addon-onboarding",
+//     "@chromatic-com/storybook",
+//     "@storybook/experimental-addon-test",
+//     "@storybook/addon-themes",
+//   ],
+//   framework: {
+//     name: "@storybook/nextjs",
+//     options: {},
+//   },
+//   staticDirs: ["../public"],
+// };
+
+// export default config;
---
+++ .storybook/vitest.setup.ts
@@ -0,0 +1,10 @@
+// TODO: Uncomment this entire file when Storybook supports Next.js 16
+// This file sets up Vitest for Storybook testing
+
+/*
+import { beforeAll } from "vitest";
+
+beforeAll(() => {
+  // Add any setup code needed for Storybook tests
+});
+*/
